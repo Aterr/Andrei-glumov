fe88b388e4023ec5a7c7d57dd52af290c20dd6c3
Aterr <bazilblack@gmail.com>
2022-04-04 02:01:06 +0300

    misc

---
+++ folder-app/pages/index.js
@@ -0,0 +1,15 @@
+import MainContainer from "../components/MainContainer";
+import styles from '../styles/index.module.scss';
+
+const Index = () => {
+    return (
+        <MainContainer>
+            <div className={styles.description}>
+                <h1>Filtred App</h1>
+            </div>
+        </MainContainer>
+
+    )
+}
+
+export default Index;
